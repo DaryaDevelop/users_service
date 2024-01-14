5a1b41301504deecfd548b3a8d10be11363a1ce6
Alexsei Popov <popov2895@gmail.com>
2024-01-14 17:07:08 +0300

    add sign_in function

--- users_service/__init__.py
+++ users_service/__init__.py
@@ -1,3 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+
 
 app = Flask(__name__)
+
+
+# Регистрация
+# Получение логина-пароля
+# Проверка на уникальность логина -> db_service (/user/<login>)
+# Сохранение логина-пароля в базу данных
+@app.post("/sign_in")
+def sign_in():
+    login = request.json.get("login")
+    password = request.json.get("password")
+    response = requests.get(f"http://{4}:{4}/user/{login}") # запрос на db_service
+    if response.json().is_exist is True:
+        return {"is_exist": True}, 400
+    # запрос на db_service на создание пользователя
+
+
+# Авторизация
+
+
+# Сменить пароль (PUT)
+
+# Удалить аккаунт (DELETE)
+
